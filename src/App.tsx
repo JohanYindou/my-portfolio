73102f5642e4026610b3e195ab24953008e64e5a
Johan Yindou <jyindou@gmail.com>
2025-09-16 20:01:47 +0200

    Début de la mise en place des routes + Ajout des résaux sociaux

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,36 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client'; // <-- Ajoutez cette ligne
 import { CssBaseline } from '@mui/material';
 import { ThemeProviderWrapper } from './context/ThemeContext.tsx';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.tsx';
 import Home from './pages/Home.tsx';
+// import About from './pages/About.tsx';
+// import Projets from './pages/Projets.tsx';
+// import Resume from './pages/Resume.tsx';
+// import Contact from './pages/Contact.tsx';
+import Footer from './components/Footer.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProviderWrapper>
     <CssBaseline />
     <StrictMode>
-      <Home />
+      <Router>
+        <Navbar />
+        {/* --- A faire 
+        Mettre en place un redirection vers Home 
+        si l'utlisateur est sur une page qui n'existe pas 
+        + Pop Up de page non trouvée, vous avez été redirigé  
+        --- */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/about' element={<About />} />
+          <Route path='/projets' element={<Projets />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/contact' element={<Contact />} /> */}
+        </Routes>
+        <Footer />
+      </Router>
     </StrictMode>
   </ThemeProviderWrapper>
 );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,98 @@
 import React, { useContext } from 'react';
 import { AppBar, Toolbar, Box, Button, IconButton, Typography } from '@mui/material';
 import { LightMode, DarkMode } from '@mui/icons-material';
-import { ColorModeContext } from '../context/ThemeContext'; // Ajustez le chemin si nécessaire
+import { ColorModeContext } from '../context/ThemeContext';
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { useNavigate,Link } from 'react-router-dom';
+
+
 
 const Navbar: React.FC = () => {
   const { toggleColorMode, mode } = useContext(ColorModeContext);
+  const navigate = useNavigate();
 
   return (
     <AppBar position='static' color='transparent' elevation={0}>
-      <Toolbar sx={{ justifyContent: 'space-between' }}>
+      <Toolbar
+        sx={{
+          position: 'relative',
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+          minHeight: 64,
+        }}>
         {/* Partie gauche : Nom */}
-        <Typography
-          variant='h6'
-          component='h1'
-          sx={{
-            fontWeight: 'bold',
-            letterSpacing: 1.5,
-            fontFamily: 'Montserrat, sans-serif',
-            color: 'primary.main',
-            textTransform: 'uppercase',
-            cursor: 'default',
-          }}>
-          Johan Yindou
-        </Typography>
+        <a href='/' className='logo'>
+          <Typography
+            variant='h6'
+            onClick={() => navigate('/')}
+            component='h1'
+            sx={{
+              fontWeight: 'bold',
+              letterSpacing: 1.5,
+              fontFamily: 'Montserrat, sans-serif',
+              color: 'primary.main',
+              textTransform: 'uppercase',
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                navigate('/');
+              }
+            }}>
+            Johan Yindou
+          </Typography>
+        </a>
 
         {/* Partie centrale : Navigation */}
-        <Box sx={{ display: 'flex', gap: 4 }}>
-          <Button color='inherit'>About</Button>
-          <Button color='inherit'>Projets</Button>
-          <Button color='inherit'>Resume</Button>
-          <Button color='inherit'>Contact</Button>
+        <Box
+          sx={{
+            position: 'absolute',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            display: 'flex',
+            gap: 5,
+          }}>
+          <Button color='inherit' component={Link} to='/about'>About</Button>
+          <Button color='inherit' component={Link} to='/projets'>Projets</Button>
+          <Button color='inherit' component={Link} to='/resume'>Resume</Button>
+          <Button color='inherit' component={Link} to='/contact'>Contact</Button>
+        </Box>
+
+        {/* Partie droite : Reseaux sociaux */}
+        <Box
+          sx={{
+            display: 'flex',
+            gap: 2,
+            alignItems: 'center',
+            marginLeft: 'auto',
+          }}>
+          <a
+            href='https://www.instagram.com/johandlag/'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <IconButton color='inherit'>
+              <FaInstagram size={20} />
+            </IconButton>
+          </a>
+
+          <a
+            href='https://www.linkedin.com/in/JohanYindou/'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <IconButton color='inherit'>
+              <FaLinkedin size={20} />
+            </IconButton>
+          </a>
+
+          <a
+            href='https://github.com/JohanYindou'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <IconButton color='inherit' sx={{ marginRight: 6 }}>
+              <FaGithub size={20} />
+            </IconButton>
+          </a>
         </Box>
 
         {/* Partie droite : Bouton mode sombre */}
