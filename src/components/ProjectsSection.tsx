6d33095287fd05bbfb0f42eed53b0ab58fad3a89
Johan Yindou <jyindou@gmail.com>
2025-09-16 19:39:13 +0200

    Ajout des composants Footer HeaderSection et ProjectsSection

---
+++ src/components/ProjectsSection.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Container,
+  Box,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+} from '@mui/material';
+import { projects } from '../data/projects';
+
+/* 
+--- 
+Faut que j'upgrade la qualité des grids
+---
+*/
+
+const ProjectsSection: React.FC = () => {
+  return (
+    <Box sx={{ backgroundColor: 'action.hover', py: 8 }}>
+      <Container maxWidth='lg' sx={{ my: 8 }}>
+        <Typography
+          variant='h4'
+          component='h2'
+          sx={{ textAlign: 'center', mb: 4 }}>
+          Derniers Projets
+        </Typography>
+        <Grid container spacing={4} justifyContent='center'>
+          {projects.map((project) => (
+            <Grid key={project.id}>
+              <Card
+                sx={{
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}>
+                <CardMedia
+                  component='img'
+                  height='200'
+                  image={project.image}
+                  alt={`Screenshot du projet ${project.name}`}
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant='h5' component='h3'>
+                    {project.name}
+                  </Typography>
+                  <Typography variant='body2' color='text.secondary'>
+                    Technologies : {project.technologies.join(', ')}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default ProjectsSection;
